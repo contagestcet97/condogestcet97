ab3e2d956aa322faff1b9e205f1bf5367d625bf2
simorai <109289730+simorai@users.noreply.github.com>
2025-06-23 15:32:50 +0100

    Initial commit

---
+++ condogestcet97.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace condogestcet97.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
